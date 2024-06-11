713b8d43291e1ef8663e0a70185b903ae3887d5a
sant <santoryo.evergarden@gmail.com>
2024-06-11 22:57:27 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="h-37.5px flex items-center justify-between -mt-2">
+	<div class="w-fit text-neutral-500">lucy.dev</div>
+	<div class="flex gap-x-4 text-neutral-500">
+		<a href="https://github.com/santowyo" class="hover:text-neutral-700">github</a>
+		<a href="https://twitter.com/santowyo" class="hover:text-neutral-700">twitter</a>
+	</div>
+</div>
+
+<main class="prose text-neutral-200">
+	<small class="text-neutral-500">IPA /ˈluːsi/ — aka lucyna, sant, santowyo</small>
+	<p>
+		The ship name is derived from the first letters of Ganyu's name and the last four letters of
+		Keqing's name. However, it's worth noting that the Chinese characters used for the ship name are
+		drastically different from the ones used for the concept of 'Gănging' (Bia). Gănging (Bi) holds
+		significant importance in Chinese culture, and it is loosely translated as "feeling." It refers
+		to a friendship-like feeling that develops between two people, groups, or business partners as
+		their relationship deepens. Despite the connection to this concept, the ship name itself is
+		based purely on the characters' names.
+	</p>
+
+	<p>
+		The popularity of Ganqing soared within the Genshin Impact fandom due to several factors.
+		Firstly, the game developer, miHoYo, often drops hints and crumbs that fuel fans' enthusiasm for
+		certain character pairings. The interactions and chemistry between Ganyu and Keging in the game
+		have led fans to imagine them as a potential romantic couple or close friends, adding to the
+		allure of the ship. Both Ganyu and Keging are members of the Liyue Qixing, a group of seven
+		highly influential individuals in Liyue. Their affiliation with this prominent organization also
+		contributes to their fame and shared experiences, making their interactions all the more
+		intriguing.
+	</p>
+
+	<p>
+		Interestingly, the contrasting opinions regarding Rex Lapis, a central character in the game,
+		have inspired fans to create light-hearted rivalry stories between Ganyu and Keging. These
+		stories often explore how the two characters learn to understand each other better, as hinted in
+		their voicelines.
+	</p>
+
+	<p>
+		Another aspect that fans adore is the combination of their purple and blue aesthetics, which has
+		led to the creation of numerous fan arts portraying the characters together. A playful reference
+		to the Ganqing ship can be found in Keging's second birthday mail, where she receives qingxin
+		flowers, which happen to be one of Ganyu's character ascension materials. This detail further
+		strengthens the connection between the two characters and adds to the ship's appeal.
+	</p>
+
+	<p>
+		Even Genshin Impact's official Twitter account playfully joined in on the fun. On February 11,
+		2022, they jokingly replied with "Ganqing Impact?" to a comment on their official art post,
+		featuring the two characters skiing with Paimon. However, the tweet was later deleted due to an
+		error in the art, where Ganyu's right ski pole seemed to be floating outside her grasp.
+	</p>
+
+	<p>
+		Interestingly, another nod to the Ganqing ship can be found in Keging's Friendly Fracal card
+		deck when invited to play the Genius Invocation TCG. The deck features both cards of herself and
+		Ganyu, a subtle yet delightful reference for fans.
+	</p>
+
+	<p>
+		In conclusion, Ganqing is a beloved ship in the Genshin Impact community, driven by the
+		captivating dynamic between Ganyu and Keging and the charming aesthetics they bring when
+		together. While its name holds no direct connection to the concept of Gănging in Chinese
+		culture, it continues to inspire fans' creativity and affection for this intriguing pairing.
+	</p>
+
+    <i>There you go, I tried to make it as short as possible ^_^</i>
+</main>
